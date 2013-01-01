d717fe6bbf618212a95fcd4210e0416e90547866
Jim McDonald <Jim@mcdee.net>
2013-01-01 23:31:54 +0000

    Added more features

--- wealdtech-core/src/main/java/com/wealdtech/jackson/ObjectMapperConfiguration.java
+++ wealdtech-core/src/main/java/com/wealdtech/jackson/ObjectMapperConfiguration.java
@@ -1,7 +1,5 @@
 package com.wealdtech.jackson;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,37 +11,71 @@ import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.wealdtech.jackson.modules.WealdJodaModule;
 
 /**
- * The configuration for an object mapper.
- * This contains various settings that can be changed for an object mapper.
+ * The configuration for an object mapper. This contains various settings that
+ * can be changed for an object mapper.
  */
 public class ObjectMapperConfiguration
 {
-  private List<Module> modules;
-  private Map<JsonParser.Feature, Boolean> parserFeatures;
-
-  private InjectableValues.Std injectableValues;
   private Optional<JsonFactory> factory;
+
+  private final List<Module> modules;
+  private final Map<JsonParser.Feature, Boolean> parserFeatures;
+
+  private final InjectableValues.Std injectableValues;
   private Optional<? extends PropertyNamingStrategy> propertyNamingStrategy;
   private Optional<JsonInclude.Include> serializationInclusion;
-  
+
   /**
-   * Create a new object mapper configuration,
-   * with suitable defaults.
+   * Create a new object mapper configuration, with suitable defaults.
    */
   public ObjectMapperConfiguration()
   {
-    this.modules = new ArrayList<>();
-    this.parserFeatures = new HashMap<>();
+    this.factory = Optional.absent();
+    this.modules = Lists.newArrayList();
+    this.parserFeatures = Maps.newHashMap();
     this.parserFeatures.put(JsonParser.Feature.ALLOW_COMMENTS, true);
     this.injectableValues = new InjectableValues.Std();
     this.modules.add(new GuavaModule());
     this.modules.add(new WealdJodaModule());
-    this.factory = Optional.absent();
     this.propertyNamingStrategy = Optional.fromNullable(new LcStrategy());
     this.serializationInclusion = Optional.fromNullable(JsonInclude.Include.NON_NULL);
+  }
+
+  /**
+   * Set the parsing factory.
+   * 
+   * @param factory
+   *          the parsing factory
+   */
+  public void setFactory(final JsonFactory factory)
+  {
+    this.factory = Optional.fromNullable(factory);
+  }
+
+  /**
+   * Get the (optional) parsing factory.
+   * 
+   * @return the parsing factory
+   */
+  public Optional<JsonFactory> getFactory()
+  {
+    return this.factory;
+  }
+
+  /**
+   * Set the property naming strategy.
+   * 
+   * @param factory
+   *          the property naming strategy
+   */
+  public void setPropertyNamingStrategy(final PropertyNamingStrategy propertyNamingStrategy)
+  {
+    this.propertyNamingStrategy = Optional.fromNullable(propertyNamingStrategy);
   }
 
   /**
@@ -53,10 +85,12 @@ public class ObjectMapperConfiguration
   {
     this.modules.clear();
   }
-  
+
   /**
    * Add an additional module.
-   * @param module the additional module
+   * 
+   * @param module
+   *          the additional module
    */
   public void addModule(final Module module)
   {
@@ -65,6 +99,7 @@ public class ObjectMapperConfiguration
 
   /**
    * Get the additional modules.
+   * 
    * @return the additional modules
    */
   public List<Module> getModules()
@@ -79,11 +114,14 @@ public class ObjectMapperConfiguration
   {
     this.parserFeatures.clear();
   }
-  
+
   /**
    * Add a parser feature.
-   * @param feature the parser feature
-   * @param value the value of the feature
+   * 
+   * @param feature
+   *          the parser feature
+   * @param value
+   *          the value of the feature
    */
   public void addParserFeature(final JsonParser.Feature feature, final Boolean value)
   {
@@ -92,6 +130,7 @@ public class ObjectMapperConfiguration
 
   /**
    * Get the parser features.
+   * 
    * @return the parser features
    */
   public Map<JsonParser.Feature, Boolean> getParserFeatures()
@@ -101,8 +140,11 @@ public class ObjectMapperConfiguration
 
   /**
    * Add an injectable value.
-   * @param name the name of the injectable
-   * @param value the value of the injectable
+   * 
+   * @param name
+   *          the name of the injectable
+   * @param value
+   *          the value of the injectable
    */
   public void addInjectableValue(final String name, final Object value)
   {
@@ -111,6 +153,7 @@ public class ObjectMapperConfiguration
 
   /**
    * Get the injectable values.
+   * 
    * @return the injectable values
    */
   public InjectableValues getInjectableValues()
@@ -119,34 +162,8 @@ public class ObjectMapperConfiguration
   }
 
   /**
-   * Set the parsing factory.
-   * @param factory the parsing factory
-   */
-  public void setFactory(final JsonFactory factory)
-  {
-    this.factory = Optional.fromNullable(factory);
-  }
-
-  /**
-   * Get the (optional) parsing factory.
-   * @return the parsing factory
-   */
-  public Optional<JsonFactory> getFactory()
-  {
-    return this.factory;
-  }
-
-  /**
-   * Set the property naming strategy.
-   * @param factory the property naming strategy
-   */
-  public void setPropertyNamingStrategy(final PropertyNamingStrategy propertyNamingStrategy)
-  {
-    this.propertyNamingStrategy = Optional.fromNullable(propertyNamingStrategy);
-  }
-
-  /**
    * Get the (optional) property naming strategy.
+   * 
    * @return the property naming strategy
    */
   public Optional<? extends PropertyNamingStrategy> getPropertyNamingStrategy()
@@ -154,10 +171,11 @@ public class ObjectMapperConfiguration
     return this.propertyNamingStrategy;
   }
 
-
   /**
    * Set the serialization inclusion.
-   * @param factory the serialization inclusion
+   * 
+   * @param factory
+   *          the serialization inclusion
    */
   public void setSerializationInclusion(final JsonInclude.Include serializationInclusion)
   {
@@ -166,6 +184,7 @@ public class ObjectMapperConfiguration
 
   /**
    * Get the (optional) serialization inclusion.
+   * 
    * @return the serialization inclusion
    */
   public Optional<JsonInclude.Include> getSerializationInclusion()
