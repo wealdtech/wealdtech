361f5f771f55e01813381bdf9ae958fc0aa547f4
Jim McDonald <Jim@mcdee.net>
2013-01-03 20:53:32 +0000

    Added Jersey provideers

--- wealdtech-jersey-server/src/main/java/com/wealdtech/jersey/providers/ObjectMapperProvider.java
+++ wealdtech-jersey-server/src/main/java/com/wealdtech/jersey/providers/ObjectMapperProvider.java
@@ -1,3 +1,18 @@
+/*
+ *    Copyright 2013 Weald Technology Trading Limited
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package com.wealdtech.jersey.providers;
 
 import javax.ws.rs.ext.ContextResolver;
@@ -9,9 +24,10 @@ import com.wealdtech.jackson.ObjectMapperFactory;
 
 /**
  * Provide an objectmapper.
- * <p/>The default objectmapper will be provided, unless an object mapper
- * configuration is passed in to the constructor for this provider in which
- * case those configuration options will override the default.
+ * <p/>
+ * The default objectmapper will be provided, unless an object mapper
+ * configuration is passed in to the constructor for this provider in which case
+ * those configuration options will override the default.
  */
 @Provider
 public class ObjectMapperProvider implements ContextResolver<ObjectMapper>
