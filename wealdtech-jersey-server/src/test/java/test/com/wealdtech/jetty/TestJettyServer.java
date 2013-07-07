9cb95d5ddcbddaa62ac9425be5b5fccff73fc9ec
Jim McDonald <Jim@mcdee.net>
2013-07-07 21:56:47 +0100

    Tidy ups and testing

--- wealdtech-jersey-server/src/test/java/test/com/wealdtech/jetty/TestJettyServer.java
+++ wealdtech-jersey-server/src/test/java/test/com/wealdtech/jetty/TestJettyServer.java
@@ -16,72 +16,23 @@
 
 package test.com.wealdtech.jetty;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.DefaultServlet;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import test.com.wealdtech.config.ApplicationModule;
 
 import com.google.inject.Guice;
-import com.google.inject.Inject;
 import com.google.inject.Injector;
-import com.google.inject.servlet.GuiceFilter;
-import com.google.inject.servlet.GuiceServletContextListener;
 import com.wealdtech.jersey.guice.JerseyServletModule;
 import com.wealdtech.jetty.JettyServer;
 
 /**
- * A simple Jetty container to test Weald Jetty and Jersey changes
+ * A simple Jetty container to test Weald Jetty and Jersey servers
  */
 public class TestJettyServer
 {
-  private static final Logger LOGGER = LoggerFactory.getLogger(TestJettyServer.class);
-
-  private transient Server server;
-
-  private transient final Injector injector;
-
-  @Inject
-  public TestJettyServer(final Injector injector)
-  {
-    this.injector = injector;
-  }
-
-  public void start() throws Exception // NOPMD
-  {
-    final int port = 8080;
-    LOGGER.info("Starting http server on port {}", port);
-    this.server = new Server();
-
-    final ServletContextHandler context = new ServletContextHandler(this.server, "/");
-    context.addEventListener(new GuiceServletContextListener()
-    {
-      @Override
-      protected Injector getInjector()
-      {
-        return TestJettyServer.this.injector;
-      }
-    });
-    context.addFilter(GuiceFilter.class, "/*", null);
-    context.addServlet(DefaultServlet.class, "/");
-
-    this.server.start();
-  }
-
-  public void join() throws Exception
-  {
-    this.server.join();
-  }
-
-  public void stop() throws Exception
-  {
-    this.server.stop();
-  }
-
   public static void main(final String[] args) throws Exception
   {
     // Create an injector with our basic configuration
-    final Injector injector = Guice.createInjector(new JerseyServletModule("test.com.wealdtech.jersey.resources"));
+    final Injector injector = Guice.createInjector(new ApplicationModule("config-multi.json"),
+                                                   new JerseyServletModule("test.com.wealdtech.jersey.resources"));
     final JettyServer webserver = injector.getInstance(JettyServer.class);
     webserver.start();
     webserver.join();
