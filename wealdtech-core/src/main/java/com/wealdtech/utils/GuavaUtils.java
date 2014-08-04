360be451053b2680508120eac498043e4557801d
Jim McDonald <Jim@mcdee.net>
2014-08-04 23:18:36 +0100

    Added emptyToNull() for maps

--- wealdtech-core/src/main/java/com/wealdtech/utils/GuavaUtils.java
+++ wealdtech-core/src/main/java/com/wealdtech/utils/GuavaUtils.java
@@ -1,6 +1,9 @@
 package com.wealdtech.utils;
 
+import com.google.common.collect.Multimap;
+
 import java.util.Collection;
+import java.util.Map;
 
 public class GuavaUtils
 {
@@ -12,4 +15,23 @@ public class GuavaUtils
     }
     return cl;
   }
+
+  public static <T, U> Map<T, U> emptyToNull(final Map<T, U> input)
+  {
+    if (input == null || input.isEmpty())
+    {
+      return null;
+    }
+    return input;
+  }
+
+  public static <T, U> Multimap<T, U> emptyToNull(final Multimap<T, U> input)
+  {
+    if (input == null || input.isEmpty())
+    {
+      return null;
+    }
+    return input;
+  }
+
 }
