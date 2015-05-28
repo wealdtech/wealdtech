20e1771bffeb6bf886f7a0634b9ce819a063c6fc
Jim McDonald <Jim@mcdee.net>
2015-05-29 00:15:09 +0100

    Added docs

--- wealdtech-services/google/src/main/java/com/wealdtech/services/google/GooglePlacesService.java
+++ wealdtech-services/google/src/main/java/com/wealdtech/services/google/GooglePlacesService.java
@@ -1,6 +1,6 @@
 package com.wealdtech.services.google;
 
-import com.wealdtech.WObject;
+import com.wealdtech.GenericWObject;
 import retrofit.http.GET;
 import retrofit.http.Query;
 
@@ -11,19 +11,31 @@ public interface GooglePlacesService
 {
   /**
    * Obtain list of autocomplete options
+   *
+   * @param key the Google key
+   * @param input the text input
+   * @param language the language to use by preference when returning places
+   * @param location any existing location information
+   * @param types the types of places that should be returned as autocomplete candidates
+   *
+   * @return
    */
   @GET("/autocomplete/json?radius=50000")
-  WObject<WObject> autocomplete(@Query("key") final String key,
-                                @Query("input") final String input,
-                                @Query("language") final String language,
-                                @Query(value="location", encodeValue=false) final String location,
-                                @Query("types") final String types);
+  GenericWObject autocomplete(@Query("key") final String key,
+                              @Query("input") final String input,
+                              @Query("language") final String language,
+                              @Query(value = "location", encodeValue = false) final String location,
+                              @Query("types") final String types);
 
   /**
    * Obtain full information on a place
+   *
+   * @param key the Google key
+   * @param placeId the Id of the place returned by the autocomplete command
+   * @param language the language to use by preference when returning places
    */
   @GET("/details/json")
-  WObject<WObject> details(@Query("key") final String key,
-                           @Query("placeid") final String placeId,
-                           @Query("language") final String language);
+  GenericWObject details(@Query("key") final String key,
+                         @Query("placeid") final String placeId,
+                         @Query("language") final String language);
 }
