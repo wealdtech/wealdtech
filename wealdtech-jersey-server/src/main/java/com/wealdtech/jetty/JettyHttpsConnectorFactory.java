6596c3394e71ec106fc8b7a08a6a207737ad7dd6
Jim McDonald <Jim@mcdee.net>
2016-07-14 10:52:19 +0100

    Disable metrics for HTTPS connections

--- wealdtech-jersey-server/src/main/java/com/wealdtech/jetty/JettyHttpsConnectorFactory.java
+++ wealdtech-jersey-server/src/main/java/com/wealdtech/jetty/JettyHttpsConnectorFactory.java
@@ -10,24 +10,14 @@
 
 package com.wealdtech.jetty;
 
+import com.wealdtech.jetty.config.JettyConnectorConfiguration;
 import org.eclipse.jetty.http.HttpVersion;
 import org.eclipse.jetty.io.ByteBufferPool;
-import org.eclipse.jetty.server.ConnectionFactory;
-import org.eclipse.jetty.server.HttpConfiguration;
-import org.eclipse.jetty.server.HttpConnectionFactory;
-import org.eclipse.jetty.server.SecureRequestCustomizer;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.SslConnectionFactory;
+import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.util.thread.ScheduledExecutorScheduler;
 import org.eclipse.jetty.util.thread.Scheduler;
 import org.eclipse.jetty.util.thread.ThreadPool;
-
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.jetty9.InstrumentedConnectionFactory;
-import com.wealdtech.jetty.config.JettyConnectorConfiguration;
-import com.wealdtech.utils.WealdMetrics;
 
 /**
  * A Jetty connector factory using the HTTPS protocol and building on the HTTP connector.
@@ -50,11 +40,12 @@ public class JettyHttpsConnectorFactory extends JettyHttpConnectorFactory
     // Set up the SSL connection factory
     final SslConnectionFactory sslConnectionFactory = new SslConnectionFactory(sslContextFactory, HttpVersion.HTTP_1_1.toString());
 
-    // Instrument the connections
-    final String timerName = MetricRegistry.name(HttpConnectionFactory.class, configuration.getBindHost(), Integer.toString(configuration.getPort()), "connections");
-    final ConnectionFactory instrumentedConnectionFactory = new InstrumentedConnectionFactory(httpConnectionFactory, WealdMetrics.getMetricRegistry().timer(timerName));
-    // And create the connection itself
-    return buildConnector(server, threadPool, scheduler, bufferPool, name, configuration, sslConnectionFactory, instrumentedConnectionFactory);
+//    // Instrument the connections
+//    final String timerName = MetricRegistry.name(HttpConnectionFactory.class, configuration.getBindHost(), Integer.toString(configuration.getPort()), "connections");
+//    final ConnectionFactory instrumentedConnectionFactory = new InstrumentedConnectionFactory(httpConnectionFactory, WealdMetrics.getMetricRegistry().timer(timerName));
+//    // And create the connection itself
+//    return buildConnector(server, threadPool, scheduler, bufferPool, name, configuration, sslConnectionFactory, instrumentedConnectionFactory);
+        return buildConnector(server, threadPool, scheduler, bufferPool, name, configuration, sslConnectionFactory);
   }
 
   @Override
